de09fa3f538416388bcc190076814870abf96115
Daniel Gustaw <gustaw.daniel@gmail.com>
2023-03-28 15:21:57 +0900

    basic implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use serde_json::{Map, Value};
+
+pub fn json_merge_patch(target: &mut Value, patch: &Value) {
+    match patch {
+        Value::Object(patch_obj) => {
+            if !target.is_object() {
+                *target = Value::Object(Map::new());
+            }
+
+            if let Value::Object(target_obj) = target {
+                for (key, value) in patch_obj {
+                    if value.is_null() {
+                        target_obj.remove(key);
+                    } else {
+                        let target_value = target_obj
+                            .entry(key.clone())
+                            .or_insert_with(|| Value::Null);
+                        json_merge_patch(target_value, value);
+                    }
+                }
+            }
+        }
+        _ => *target = patch.clone(),
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +29,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_json_merge_patch() {
+        let mut target = serde_json::from_str(r#"{"a": "b", "c": {"d": "e", "f": "g"}}"#).unwrap();
+        let patch = serde_json::from_str(r#"{"a": "z", "c": {"f": null}}"#).unwrap();
+
+        json_merge_patch(&mut target, &patch);
+
+        let expected: serde_json::Value = serde_json::from_str(r#"{"a": "z", "c": {"d": "e"}}"#).unwrap();
+        assert_eq!(target, expected);
     }
 }
